1da0f5c4b158059eeb3de90b70a0a708b9f86958
Tyler Begrow <Tyler.Begrow@pb.com>
2025-02-26 05:41:27 -0800

    go needs to be on client and server. RPC works properly in docker

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -45,8 +47,13 @@ func main() {
 	app := Config {
 		Models: data.New(client),
 	}
-	//start web server
-	//go app.serve()
+
+	//register the RPC Server, tell it that it will accept the requests
+	//start rpc server
+	err = rpc.Register(new(RPCServer))
+
+	go app.rpcListen()
+
 	log.Println("Starting service on port", webPort)
 	srv := &http.Server {
 		Addr: fmt.Sprintf(":%s", webPort),
@@ -60,16 +67,22 @@ func main() {
 	
 }
 
-// func (app *Config) serve() {
-// 	srv := &http.Server {
-// 		Addr: fmt.Sprintf(":%s", webPort),
-// 		Handler: app.routes(),
-// 	}
-// 	err := srv.ListenAndServe()
-// 	if err != nil {
-// 		log.Panic()
-// 	}
-// }
+func (app *Config) rpcListen() error {
+	log.Println("Starting RPC server on port ", rpcPort)
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+}
 
 func connectToMongo() (*mongo.Client, error) {
 	//create connection options
